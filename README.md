12b293f1f6ca6d28677a6b7d469b62a2282a2945
t-cook <t-cook@microsoft.com>
2018-04-26 17:21:16 -0700

    finished 6

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 [link](http://www.pythonchallenge.com/)
 
-# Highest Level: 6
-[Current Level](http://www.pythonchallenge.com/pc/def/channel.html)
+# Highest Level: 7
+[Current Level](http://www.pythonchallenge.com/pc/def/oxygen.html)
--- challenge6/python_challenge6.py
+++ challenge6/python_challenge6.py
@@ -1,11 +1,35 @@
-import requests
-from bs4 import BeautifulSoup
 
-import pickle
-from pprint import pprint
+from zipfile import ZipFile
 
-# Collect first page of artists’ list
-page = requests.get('http://www.pythonchallenge.com/pc/def/channel.html')
+num = 90052 
+counter = 0
+result = None
+all_comments = b""
+path = []
+with ZipFile("channel.zip", 'r') as testzip:
 
-soup = BeautifulSoup(page.text, "html.parser")
-soup = soup.get_text().replace("\n", "")
+    while(num != 0):
+        counter += 1
+
+        file_name = str(num) + ".txt"
+        path.append(file_name)
+        with open(file_name, "r") as myfile:
+            data = myfile.readline()
+
+        num = data.split()[-1]
+
+        try:
+            num = int(num)
+        except:
+            result = num
+            num = 0
+
+#print (file_name)
+
+list_inside = testzip.infolist()
+for name in path:
+    for inside in list_inside:
+        if inside.filename == name:
+            all_comments += inside.comment
+
+print(all_comments.decode("utf-8"))
