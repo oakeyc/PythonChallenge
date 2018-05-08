969f4e13f930d3d03e1a0604ea09a781d454e089
t-cook <t-cook@microsoft.com>
2018-05-08 13:59:49 -0700

    challenge 7 completed (with help)

---
+++ challenge7/python_challenge7.py
@@ -0,0 +1,21 @@
+from PIL import Image
+from pprint import pprint
+img = Image.open('oxygen.png')
+
+pix = img.load() # pix is a list of (R,G,B,A), which can be accessed by pix[x,y]
+
+print(img.size)# (width, height)
+
+center = [pix[x,img.size[1]/2] for x in range(30)] 
+
+pprint(center)
+
+res = [chr(pix[4,47][0])] + [chr(pix[5+7*i,47][0]) for i in range(86)]
+x = ''.join(res) 
+
+print (x)
+
+# nums you get from the top thing
+nums = [105, 110, 116, 101, 103, 114, 105, 116, 121]
+for num in nums:
+    print(chr(num)) 
